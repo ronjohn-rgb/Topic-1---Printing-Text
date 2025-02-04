33f32e9331013f974fb1e417d58af3672dbbebcd
701035287 <701035287@2260I-L29-022>
2025-02-04 14:01:26 -0500

    Add project files.

---
+++ Topic 1 - Printing Text/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Topic_1___Printing_Text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Aron John E. Bautista
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I will print on a new line.");
+
+            Console.WriteLine(" ");
+
+            Console.Write("Hello, World!");
+            Console.Write(" This will print on the same line.");
+
+
+
+            Console.WriteLine("Something");
+        }
+    }
+}
